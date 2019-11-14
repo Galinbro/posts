55e5f5decddc40c00ef1474710416c5ae403da51
Usuario Energy <usuario_energy@grupoplenum.local>
2019-11-14 10:02:22 -0600

    Todo el modulo de categorias funcionando

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -6,18 +6,24 @@
 
     <div class="row">
         <div class="col-sm-6">
-            {!! Form::model($category,['method' =>'PATCH','action'=>['AdminCategoriesController@store', $category->id]]) !!}
+            {!! Form::model($category,['method' =>'PATCH','action'=>['AdminCategoriesController@update', $category->id]]) !!}
 
             <div class="form-group">
                 {!! Form::label('name', 'Nombre:') !!}
                 {!! Form::text('name', null, ['class'=>'form-control']) !!}
             </div>
             <div class="form-group">
-                {!! Form::submit('Actualizar categoria', ['class'=>'btn btn-primary']) !!}
+                {!! Form::submit('Actualizar categoria', ['class'=>'btn btn-primary col-sm-6']) !!}
             </div>
             {!! Form::close() !!}
-        </div>
 
+            {!! Form::open(['method' =>'DELETE','action'=>['AdminCategoriesController@destroy', $category->id]]) !!}
+            <div class="form-group">
+                {!! Form::submit('Borrar Categoria', ['class'=>'btn btn-danger col-sm-6']) !!}
+            </div>
+            {!! Form::close() !!}
+
+        </div>
     </div>
 
 @endsection
